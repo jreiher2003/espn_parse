c1cb4af7fa8dee80de6c65acd9269c12db6550a6
Jeff Reiher <jeffreiher@gmail.com>
2015-04-17 19:08:07 -0400

    Add parse and analysis files

---
+++ nba_analysis.py
@@ -0,0 +1,199 @@
+import scipy.stats
+import statstools
+import pymongo
+import pprint
+import numpy
+import itertools
+import math
+# print scipy.stats.ttest_ind(NBA.chi_home, NBA.tor_away, equal_var=False)
+def mongo_con_nba():
+	# establish a connection to the database
+	connection = pymongo.MongoClient("mongodb://localhost")
+	# get a handle to the school database
+	db=connection.sports
+	nba = db.nba
+	return nba
+nba = mongo_con_nba()
+
+def h_scores(team):
+	cur = nba.aggregate([{"$match": {"team": team}}, {"$match": {"h_a": "vs"}},{"$group": {"_id": {"scores": "$h_a"}, team: {"$push": "$s"} }}])
+	new_cur = cur['result'][0][team]
+	new_cur = filter(None, new_cur)
+	return new_cur
+
+def a_scores(team):
+	cur = nba.aggregate([{"$match": {"team": team}}, {"$match": {"h_a": "@"}},{"$group": {"_id": {"scores": "$h_a"}, team: {"$push": "$s"} }}])
+	new_cur = cur['result'][0][team]
+	new_cur = filter(None, new_cur)
+	return new_cur
+
+
+atl_away = a_scores('atl')
+atl_home = h_scores('atl')
+
+tor_away = a_scores('tor')
+tor_home = h_scores('tor')
+
+chi_away = a_scores('chi')
+chi_home = h_scores('chi')
+
+cle_away = a_scores('cle')
+cle_home = h_scores('cle')
+
+wsh_away = a_scores('wsh')
+wsh_home = h_scores('wsh')
+
+mil_away = a_scores('mil')
+mil_home = h_scores('mil')
+
+mia_away = a_scores('mia')
+mia_home = h_scores('mia')
+
+bkn_away = a_scores('bkn')
+bkn_home = h_scores('bkn')
+
+cha_away = a_scores('cha')
+cha_home = h_scores('cha')
+
+det_away = a_scores('det')
+det_home = h_scores('det')
+
+ind_away = a_scores('ind')
+ind_home = h_scores('ind')
+
+bos_away = a_scores('bos')
+bos_home = h_scores('bos')
+
+orl_away = a_scores('orl')
+orl_home = h_scores('orl')
+
+phi_away = a_scores('phi')
+phi_home = h_scores('phi')
+
+ny_away = a_scores('ny')
+ny_home = h_scores('ny')
+
+gs_away = a_scores('gs')
+gs_home = h_scores('gs')
+
+mem_away = a_scores('mem')
+mem_home = h_scores('mem')
+
+hou_away = a_scores('hou')
+hou_home = h_scores('hou')
+
+por_away = a_scores('por')
+por_home = h_scores('por')
+
+dal_away = a_scores('dal')
+dal_home = h_scores('dal')
+
+lac_away = a_scores('lac')
+lac_home = h_scores('lac')
+
+sa_away = a_scores('sa')
+sa_home = h_scores('sa')
+
+okc_away = a_scores('okc')
+okc_home = h_scores('okc')
+
+no_away = a_scores('no')
+no_home = h_scores('no')
+
+phx_away = a_scores('phx')
+phx_home = h_scores('phx')
+
+utah_away = a_scores('utah')
+utah_home = h_scores('utah')
+
+den_away = a_scores('den')
+den_home = h_scores('den')
+
+sac_away = a_scores('sac')
+sac_home = h_scores('sac')
+
+lal_away = a_scores('lal')
+lal_home = h_scores('lal')
+
+min_away = a_scores('min')
+min_home = h_scores('min')
+
+def calculate_score(data, z):
+    # remove outliers
+    # extract data between lower and upper quartile
+    data.sort()
+    lowerq = int((len(data))/4)
+    #change from 3 to 4 adds an extra game to the upper
+    upperq = lowerq * 3 + 2
+    newdata = [data[i] for i in range(lowerq,upperq)]
+    # fit Gaussian using MLE
+    mu = round(numpy.mean(newdata),2)
+    sigma = round(numpy.std(newdata),2)
+    #compute x that corresponds to standard score z
+    x = math.ceil(mu + (z * sigma))
+    y = math.floor(mu - (z * sigma))
+    return x,y,mu,sigma,newdata
+
+def ss(data):
+	mu=numpy.mean(data)
+	return sum([(x-mu)**2 for x in data])
+
+def combine_score(away, home):
+	if len(home) > len(away):
+		return map(sum, itertools.izip_longest(away, home, fillvalue=int(round(numpy.mean(away)))))
+	elif len(home) < len(away):
+		return map(sum, itertools.izip_longest(away, home, fillvalue=int(round(numpy.mean(home)))))
+	elif len(home) == len(away):
+		return map(sum, itertools.izip_longest(away, home))
+
+def total_combinations(away, home):	
+	total = []
+	for com in itertools.product(away, home):
+		x = sum(com)
+		total.insert(0,x)
+	return len(total), round(numpy.mean(total),2), round(numpy.std(total),2), round(numpy.median(total),2)
+
+def matchup(n1, away, n2,home):
+	away.sort()
+	home.sort()
+	# sp2 = (ss(away)+ss(home))/(len(away)+len(home))
+	# stdp = numpy.sqrt(sp2)
+	print "#######   " + n1.upper() + " away"
+	print away
+	print calculate_score(away, 1.65)
+	print len(away)
+	print "avg: ",round(numpy.mean(away),2)
+	print "std: ", round(numpy.std(away),2)
+	print "Under: ", math.ceil(numpy.mean(away) + numpy.std(away) * .85)
+	print "Over: ",  math.floor(numpy.mean(away) - numpy.std(away) * .85)
+	print "|--------------------------------------|"
+	print "########   " + n2.upper() + " home"
+	print home
+	print calculate_score(home, 1.65)
+	print len(home)
+	print "avg: ", round(numpy.mean(home),2)
+	print "std: ", round(numpy.std(home),2)
+	print "Under: ", math.ceil(numpy.mean(home) + numpy.std(home) * .85)
+	print "Over: ",  math.floor(numpy.mean(home) - numpy.std(home) * .85)
+	print "|--------------------------------------|"
+	print total_combinations(away, home)[0]
+	print combine_score(away, home)
+	print calculate_score(combine_score(away,home), 1.65)
+	print numpy.mean(combine_score(away, home))
+	print "Game_Total: ", total_combinations(away, home)[1], total_combinations(away, home)[3]
+	print "STD: ", total_combinations(away, home)[2]
+	print "Game_Under: ", math.ceil(total_combinations(away,home)[1] + (total_combinations(away,home)[2] * .85))
+	print "Game_Over: ", math.floor(total_combinations(away,home)[1] - (total_combinations(away,home)[2] * .85))
+	print "Actual Game Score: "
+	print "Game Totals: "
+	print ""
+	
+
+
+
+print matchup('okc', okc_away, 'chi', chi_home)
+print matchup('dal', dal_away, 'por', por_home)
+
+
+
+
---
+++ parse_espn_nba.py
@@ -0,0 +1,133 @@
+from bs4 import BeautifulSoup as bsoup
+import requests as rq
+import csv, simplejson, decimal, codecs
+import pymongo 
+import time
+import pprint
+
+
+nba_lst = ['atl', 'tor', 'chi', 'cle','wsh', 'mil','mia','bkn','cha','det',
+           'ind','bos','orl','phi','ny','gs','mem','hou','por','dal','lac',
+           'sa','okc','no','phx','utah','den','sac','lal','min']
+           
+
+def connect():
+    connection = pymongo.MongoClient("mongodb://localhost")
+    db=connection.sports
+    nba = db.nba
+    return nba
+
+def parse_espn(name):
+    url = "http://espn.go.com/nba/team/schedule/_/name/" + name
+    r = rq.get(url)
+    soup = bsoup(r.content)
+    ## Find all the rows that have classes. Remove the first one -- it's irrelevant.
+    trs = soup.find_all("tr", class_=True)[1:]
+    ## Main procedure.
+    with open("nba_2015_csv/"+name + "_2015_schedule.csv", "wb") as ofile:
+        f = csv.writer(ofile)
+        ## Write the headers. 
+        f.writerow(["team","date","a/h","opponent","w/l","s","os"])
+        for tr in trs:
+            team = name
+            tds = tr.find_all("td")
+            date = tds[0].get_text().encode("utf-8")
+            opp = tds[1].find_all("li", {"class": "team-name"})
+            for teams in opp:
+                other_team = teams.get_text()
+            opponent = tds[1].find_all('li',{'class':'game-status'})
+            for o in opponent:
+                h_a = o.get_text()
+                try:
+                    win_loss = tds[2].find_all('li',{'class':'game-status win'}) or tds[2].find_all('li',{'class':'game-status loss'})
+                    for a in win_loss:
+                        w_l = a.get_text()
+
+                    score = tds[2].find_all('a')
+                    for s in score:
+                        if w_l == 'W':
+                            gs = s.get_text().split("-")[0]
+                            gs = int(gs)
+                            ogs = s.get_text().split("-")[1].split(" ")[0]
+                            ogs = int(ogs)                            
+                        elif w_l == 'L':
+                            gs = s.get_text().split("-")[1].split(" ")[0]
+                            gs = int(gs)
+                            ogs = s.get_text().split("-")[0]
+                            ogs = int(ogs)
+                except:
+                        h_a = ''
+                        w_l = None
+                        other_team = ''
+                        gs = None
+                        ogs = None
+            ## write the result to the CSV file.
+                finally:
+                    f.writerow([team,date,h_a,other_team,w_l,gs,ogs])
+                    g = dict(date=date, team=team,h_a=h_a,other_team=other_team, w_l=w_l,s=gs,os=ogs)
+                    connect().save(g)
+                    
+
+def nba_teams(lst):
+    i = 0
+    while i < len(lst):
+        parse_espn(str(lst[i]))
+        time.sleep(2)
+        i+=1
+        
+nba_teams(nba_lst)
+    
+
+# parse_espn('atl')
+# parse_espn('tor')
+# parse_espn('chi')
+# parse_espn('cle')
+# parse_espn('wsh')
+# parse_espn('mil')
+# parse_espn('mia')
+# parse_espn('bkn')
+# parse_espn('cha')
+# parse_espn('det')
+# parse_espn('ind')
+# parse_espn('bos')
+# parse_espn('orl')
+# parse_espn('phi')
+# parse_espn('ny')
+
+# parse_espn('gs')
+# parse_espn('mem')
+# parse_espn('hou')
+# parse_espn('por')
+# parse_espn('dal')
+# parse_espn('lac')
+# parse_espn('sa')
+# parse_espn('okc')
+# parse_espn('no')
+# parse_espn('phx')
+# parse_espn('utah')
+# parse_espn('den')
+# parse_espn('sac')
+# parse_espn('lal')
+# parse_espn('min')
+
+
+def make_json(name):
+    data = open(name+"_2015_schedule.csv")
+    reader = csv.DictReader(data, delimiter=",", quotechar='"')
+    print reader
+    with codecs.open(name+"_out.json", "w", encoding="utf-8") as out:
+       for r in reader:
+          
+          for k, v in r.items(): 
+             # make sure nulls are generated
+             if not v:
+                r[k] = None
+             # parse and generate decimal arrays
+             elif k == "s":
+                r[k] = int(v)
+             # generate a number
+             elif k == "os":
+                r[k] = int(v)
+          out.write(simplejson.dumps(r, ensure_ascii=False, use_decimal=True)+"\n")
+
+
