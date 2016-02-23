9d9d7149ff0148c1b14aa90b591aadefdd624796
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-23 11:19:36 -0500

    Update parser

--- parse_espn_nba.py
+++ parse_espn_nba.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup as bsoup
 import requests as rq
-import csv, simplejson, decimal, codecs
+import csv
 import pymongo 
 import time
 import pprint
@@ -14,32 +14,32 @@ nba_lst = ['atl', 'tor', 'chi', 'cle','wsh', 'mil','mia','bkn','cha','det',
 def connect():
     connection = pymongo.MongoClient("mongodb://localhost")
     db=connection.sports
-    nba = db.nba
+    nba = db.nba15_16
     return nba
 
 def parse_espn(name):
     url = "http://espn.go.com/nba/team/schedule/_/name/" + name
     r = rq.get(url)
-    soup = bsoup(r.content)
+    soup = bsoup(r.content, "lxml")
     ## Find all the rows that have classes. Remove the first one -- it's irrelevant.
     trs = soup.find_all("tr", class_=True)[1:]
     ## Main procedure.
-    with open("nba_2015_csv/"+name + "_2015_schedule.csv", "wb") as ofile:
+    with open("nba_2015-16_csv/"+ name + "_2015-16_schedule.csv", "wb") as ofile:
         f = csv.writer(ofile)
         ## Write the headers. 
-        f.writerow(["team","date","a/h","opponent","w/l","s","os"])
+        f.writerow( ["team","date","a/h","opponent","w/l","s","os"] )
         for tr in trs:
             team = name
             tds = tr.find_all("td")
             date = tds[0].get_text().encode("utf-8")
-            opp = tds[1].find_all("li", {"class": "team-name"})
+            opp = tds[1].find_all( "li", {"class": "team-name"} )
             for teams in opp:
                 other_team = teams.get_text()
-            opponent = tds[1].find_all('li',{'class':'game-status'})
+            opponent = tds[1].find_all( 'li', {'class':'game-status'} )
             for o in opponent:
                 h_a = o.get_text()
                 try:
-                    win_loss = tds[2].find_all('li',{'class':'game-status win'}) or tds[2].find_all('li',{'class':'game-status loss'})
+                    win_loss = tds[2].find_all( 'li', {'class':'game-status win'} ) or tds[2].find_all( 'li', {'class':'game-status loss'} )
                     for a in win_loss:
                         w_l = a.get_text()
 
@@ -111,23 +111,23 @@ nba_teams(nba_lst)
 # parse_espn('min')
 
 
-def make_json(name):
-    data = open(name+"_2015_schedule.csv")
-    reader = csv.DictReader(data, delimiter=",", quotechar='"')
-    print reader
-    with codecs.open(name+"_out.json", "w", encoding="utf-8") as out:
-       for r in reader:
+# def make_json(name):
+#     data = open(name+"_2015_schedule.csv")
+#     reader = csv.DictReader(data, delimiter=",", quotechar='"')
+#     print reader
+#     with codecs.open(name+"_out.json", "w", encoding="utf-8") as out:
+#        for r in reader:
           
-          for k, v in r.items(): 
-             # make sure nulls are generated
-             if not v:
-                r[k] = None
-             # parse and generate decimal arrays
-             elif k == "s":
-                r[k] = int(v)
-             # generate a number
-             elif k == "os":
-                r[k] = int(v)
-          out.write(simplejson.dumps(r, ensure_ascii=False, use_decimal=True)+"\n")
+#           for k, v in r.items(): 
+#              # make sure nulls are generated
+#              if not v:
+#                 r[k] = None
+#              # parse and generate decimal arrays
+#              elif k == "s":
+#                 r[k] = int(v)
+#              # generate a number
+#              elif k == "os":
+#                 r[k] = int(v)
+#           out.write(simplejson.dumps(r, ensure_ascii=False, use_decimal=True)+"\n")
 
 
